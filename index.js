90012804b88184a5d0ab9221313efbea9a5b2d64
joseleh@gmail.com <joseleh@gmail.com>
2018-06-11 22:09:32 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ function logShout(string) {
     return  "I can't hear you!"
     }
   function sayHiToGrandma(string) {
-    console.log("I can't hear you!=== YES INDEED")
+    console.log("I can't hear you!)
     return "YES INDEED!"
   }
   
