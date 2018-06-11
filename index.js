c9cf26532d5aad9f8385acfe0cb5e6246f6181d0
joseleh@gmail.com <joseleh@gmail.com>
2018-06-11 21:25:32 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,12 @@
+function shout(string) { 
+  return string.toUpperCase() //'HELLO!'
+  
+}
+function whisper(string) {
+  return string.toLowerCase() //'hello'
+}
+function logShout(string) {
+  console.log("string.toUpperCase")
+  
+  
+}
